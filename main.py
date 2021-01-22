a9e1eff4056544362326ca4732a00c98c985009c
mlinhpe <mailinh.pham711@gmail.com>
2021-01-22 22:29:42 +0100

    update

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from pymongo import MongoClient
 
 
 def save_file_to_database():
-    """
+
     client = MongoClient('localhost', 27017)
     db = client['crawler-test']
     collection = db['test']
-    """
 
+    """
     client = MongoClient('mongodb://crawler:pwp21@127.0.0.1:27017/')
     db = client['crawler-pwp21']
     collection = db['test-file-import']
+    """
 
     with open('json_testfile.json') as file:
         file_data = json.load(file)
