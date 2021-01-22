5b3816f49815d1875781a195f60e5c6dd7c85810
mlinhpe <mailinh.pham711@gmail.com>
2021-01-22 23:21:05 +0100

    update

--- create_database.py
+++ create_database.py
@@ -1,4 +1,4 @@
 from pymongo import MongoClient
-client = MongoClient("mongodb://localhost:27017")
+client = MongoClient("mongodb://localhost:27018")
 db = client['crawler-test-db']
 collection = db['test']
