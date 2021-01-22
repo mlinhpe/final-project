fa021e8544b7e425dbcb6dc69cf531b0e5450664
mlinhpe <mailinh.pham711@gmail.com>
2021-01-22 22:43:10 +0100

    update

--- create_database.py
+++ create_database.py
@@ -2,3 +2,4 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017")
 db = client['crawler-test-db']
 collection = db['test']
+client.test.add_user('testuser', 'test123', roles=[{'role':'root','db':'test'}])
